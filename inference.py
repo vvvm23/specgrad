2fa35385b2f4571aa49df6dbe920e7f8bdd52de5
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2023-03-11 10:44:43 +0000

    an untested inference loop

---
+++ inference.py
@@ -0,0 +1,104 @@
+import logging
+
+from accelerate.logging import get_logger
+
+logging.basicConfig(level=logging.INFO)
+logger = get_logger(__name__)
+
+from pathlib import Path
+from typing import Optional
+
+import simple_parsing
+import soundfile
+import torch
+from accelerate import Accelerator
+from tqdm import tqdm
+
+from config import Config
+from data.preprocess import (
+    calculate_mel,
+    calculate_tf_filter,
+    get_pinv_mel_basis,
+    load_waveform,
+    transform_noise,
+)
+from model import SpecGrad
+from scheduler import SpecGradDDPMScheduler
+
+
+@torch.no_grad()
+def inference(
+    model: SpecGrad, mel_spectrogram: torch.FloatTensor, config: Config, pinv_mel_basis: Optional[torch.FloatTensor]
+):
+    waveform = torch.randn(config.data.sample_length)
+    if not pinv_mel_basis:
+        pinv_mel_basis = get_pinv_mel_basis(
+            config.data.sampling_rate, config.data.n_fft, config.data.n_mels, config.data.fmin, config.data.fmax
+        )
+
+    filter_coefficients = calculate_tf_filter(
+        mel_spectrogram, pinv_mel_basis, lifter_order=config.data.lifter_order, envelope_min=config.data.envelope_min
+    )
+
+    waveform = transform_noise(
+        filter_coefficients,
+        waveform,
+        n_fft=config.data.n_fft,
+        window_length=config.data.window_length,
+        hop_length=config.data.hop_length,
+    )
+
+    scheduler = SpecGradDDPMScheduler(
+        config.model.max_timesteps,
+        beta_start=config.training.beta_start,
+        beta_end=config.training.beta_end,
+        beta_schedule=config.training.beta_schedule,
+    )
+
+    for t in tqdm(scheduler.timesteps):
+        noise_pred = model(waveform, mel_spectrogram, t)
+        waveform = scheduler.step(noise_pred, t, waveform, filter_coefficients, config.data)
+
+    waveform = waveform.clamp(-1, 1).float().cpu().numpy()
+    waveform = waveform[:, :, : config.data.sample_length]
+    return waveform
+
+
+# TODO: really need to generally refactor to pass around dict of configs
+def main(args, config: Config):
+    accelerator = Accelerator()
+    model = SpecGrad(**vars(config.model))
+    model = accelerator.prepare(model)
+    if args.resume_dir:
+        accelerator.load_state(args.resume_dir)
+
+    waveform = load_waveform(
+        args.input_file, sr=config.data.sampling_rate, sample_length=config.data.sample_length, random_clip=False
+    )
+
+    mel_spec = calculate_mel(
+        waveform,
+        sr=config.data.sampling_rate,
+        window_length=config.data.window_length,
+        hop_length=config.data.hop_length,
+        n_mels=config.data.n_mels,
+        fmin=config.data.fmin,
+        fmax=config.data.fmax,
+    )
+
+    recon = inference(model, mel_spec, config)
+    soundfile.write("output.wav", recon, config.data.sample_length, subtype="PCM_16")
+
+
+if __name__ == "__main__":
+    parser = simple_parsing.ArgumentParser()
+    parser.add_argument("input_file", type=str)
+    parser.add_argument("--config-path", type=str, default=None)
+    parser.add_argument("--resume-dir", type=str, default=None)
+    args = parser.parse_args()
+
+    # TODO: hook config into args
+    config = Config()
+    if args.config_path:
+        config = Config.load(args.config_path)
+    main(args, config)
---
+++ scheduler.py
@@ -0,0 +1,116 @@
+import math
+from dataclasses import dataclass
+from typing import List, Optional, Tuple, Union
+
+import numpy as np
+import torch
+from diffusers.schedulers import DDPMScheduler, DDPMSchedulerOutput
+from diffusers.utils import randn_tensor
+
+from config import DataConfig
+from data.preprocess import transform_noise
+
+
+class SpecGradDDPMScheduler(DDPMScheduler):
+    def step(
+        self,
+        model_output: torch.FloatTensor,
+        timestep: int,
+        sample: torch.FloatTensor,
+        filter_coefficients: torch.FloatTensor,
+        data_config: DataConfig,
+        generator=None,
+        return_dict: bool = True,
+    ) -> Union[DDPMSchedulerOutput, Tuple]:
+        """
+        Predict the sample at the previous timestep by reversing the SDE. Core function to propagate the diffusion
+        process from the learned model outputs (most often the predicted noise).
+        Args:
+            model_output (`torch.FloatTensor`): direct output from learned diffusion model.
+            timestep (`int`): current discrete timestep in the diffusion chain.
+            sample (`torch.FloatTensor`):
+                current instance of sample being created by diffusion process.
+            generator: random number generator.
+            return_dict (`bool`): option for returning tuple rather than DDPMSchedulerOutput class
+        Returns:
+            [`~schedulers.scheduling_utils.DDPMSchedulerOutput`] or `tuple`:
+            [`~schedulers.scheduling_utils.DDPMSchedulerOutput`] if `return_dict` is True, otherwise a `tuple`. When
+            returning a tuple, the first element is the sample tensor.
+        """
+        t = timestep
+        num_inference_steps = self.num_inference_steps if self.num_inference_steps else self.config.num_train_timesteps
+        prev_t = timestep - self.config.num_train_timesteps // num_inference_steps
+
+        if model_output.shape[1] == sample.shape[1] * 2 and self.variance_type in ["learned", "learned_range"]:
+            model_output, predicted_variance = torch.split(model_output, sample.shape[1], dim=1)
+        else:
+            predicted_variance = None
+
+        # 1. compute alphas, betas
+        alpha_prod_t = self.alphas_cumprod[t]
+        alpha_prod_t_prev = self.alphas_cumprod[prev_t] if prev_t >= 0 else self.one
+        beta_prod_t = 1 - alpha_prod_t
+        beta_prod_t_prev = 1 - alpha_prod_t_prev
+        current_alpha_t = alpha_prod_t / alpha_prod_t_prev
+        current_beta_t = 1 - current_alpha_t
+
+        # 2. compute predicted original sample from predicted noise also called
+        # "predicted x_0" of formula (15) from https://arxiv.org/pdf/2006.11239.pdf
+        if self.config.prediction_type == "epsilon":
+            pred_original_sample = (sample - beta_prod_t ** (0.5) * model_output) / alpha_prod_t ** (0.5)
+        elif self.config.prediction_type == "sample":
+            pred_original_sample = model_output
+        elif self.config.prediction_type == "v_prediction":
+            pred_original_sample = (alpha_prod_t**0.5) * sample - (beta_prod_t**0.5) * model_output
+        else:
+            raise ValueError(
+                f"prediction_type given as {self.config.prediction_type} must be one of `epsilon`, `sample` or"
+                " `v_prediction`  for the DDPMScheduler."
+            )
+
+        # 3. Clip or threshold "predicted x_0"
+        if self.config.clip_sample:
+            pred_original_sample = pred_original_sample.clamp(
+                -self.config.clip_sample_range, self.config.clip_sample_range
+            )
+
+        if self.config.thresholding:
+            pred_original_sample = self._threshold_sample(pred_original_sample)
+
+        # 4. Compute coefficients for pred_original_sample x_0 and current sample x_t
+        # See formula (7) from https://arxiv.org/pdf/2006.11239.pdf
+        pred_original_sample_coeff = (alpha_prod_t_prev ** (0.5) * current_beta_t) / beta_prod_t
+        current_sample_coeff = current_alpha_t ** (0.5) * beta_prod_t_prev / beta_prod_t
+
+        # 5. Compute predicted previous sample µ_t
+        # See formula (7) from https://arxiv.org/pdf/2006.11239.pdf
+        pred_prev_sample = pred_original_sample_coeff * pred_original_sample + current_sample_coeff * sample
+
+        # 6. Add noise
+        variance = 0
+        if t > 0:
+            device = model_output.device
+            variance_noise = randn_tensor(
+                model_output.shape, generator=generator, device=device, dtype=model_output.dtype
+            )
+            variance_noise = transform_noise(
+                filter_coefficients,
+                variance_noise,
+                n_fft=data_config.n_fft,
+                window_length=data_config.window_length,
+                hop_length=data_config.hop_length,
+            )
+            if self.variance_type == "fixed_small_log":
+                variance = self._get_variance(t, predicted_variance=predicted_variance) * variance_noise
+            elif self.variance_type == "learned_range":
+                variance = self._get_variance(t, predicted_variance=predicted_variance)
+                variance = torch.exp(0.5 * variance) * variance_noise
+            else:
+                variance = (self._get_variance(t, predicted_variance=predicted_variance) ** 0.5) * variance_noise
+
+        pred_prev_sample = pred_prev_sample + variance
+
+        if not return_dict:
+            return (pred_prev_sample,)
+
+        return DDPMSchedulerOutput(prev_sample=pred_prev_sample, pred_original_sample=pred_original_sample)
